d4d0baef91ca93a468d2c518af2e3642e69c6b07
Vitali Ivanou <v.ivanou@gmail.com>
2022-07-16 14:07:58 +0200

    lessons 16.07.22

--- JS/script.js
+++ JS/script.js
@@ -1,31 +1,7 @@
-'use strict';
-function first() {
-    // do something
-    setTimeout(function() {
-        console.log(1 + ' (setTimeout())');
-    }, 500);
+"use strict";
+const hamburger = true,
+    fries = true;
+
+if (hamburger && fries) {
+    console.log('Я сыт!')
 }
-
-function second() {
-    console.log(2);
-}
-
-first();
-second();
-
-function LearnJS(lang, callback) {
-    console.log (`Я учу: ${lang}`);
-    callback();
-}
-
-LearnJS('JavaScript', function() { // вызов c анонимной функцией как параметром
-    console.log('Я прошел этот урок!');
-});
-
-/* то же самое, но вызов "осознанной" функции*/
-function done() {
-    console.log('Я прошел этот урок!');
-}
-LearnJS('JavaScript', done); //имя функции передаем как параметр
-
-
