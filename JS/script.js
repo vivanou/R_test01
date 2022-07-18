29d9b25d4f5a53661107b3f6da27bbd0c28b6acc
Vitali Ivanou <v.ivanou@gmail.com>
2022-07-18 15:00:42 +0200

    212d

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,43 @@
 "use strict";
+/*
 const hamburger = true,
     fries = true;
 
 if (hamburger && fries) {
-    console.log('Я сыт!')
+    console.log('Я сыт!');
+}
+
+console.log(hamburger && fries);
+*/
+
+/*
+const hamburger = 5;
+const fries = 1;
+
+// см таблицу приоритета операторов, === выполняется раньше &&
+if ( humburger === 3 && fries ) {
+    console.log('Все сыты!');
+} else {
+    console.log('Мы уходим!');
+}
+*/
+
+const hamburger = 5,
+    fries = 1,
+    cola = 5;
+// в консоли получаем '0', т.к. оператор && возращает числовое значение
+// последней переменной, на которой он остановил свою работу
+console.log(hamburger && cola && fries);
+
+//еще варианты:
+console.log(1 && 0);
+console.log(1 && 5);
+console.log(null && 5);
+console.log(1 && 'fjkjdflsdjl');
+
+
+if (hamburger === 3 && cola === 1 && fries) {
+    console.log('Все сыты!');
+} else {
+    console.log('Мы уходим!');
 }
